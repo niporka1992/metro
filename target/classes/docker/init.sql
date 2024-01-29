75ed5562b98b60f3fddf4cef9c917bf26bf1422a
Nick <67371365+niporka1992@users.noreply.github.com>
2024-01-29 16:57:05 +0300

    Delete target directory

--- target/classes/docker/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Route (
-                       Id INT PRIMARY KEY AUTO_INCREMENT,
-                       Code VARCHAR(255) NOT NULL,
-                       Name VARCHAR(255) NOT NULL
-);
